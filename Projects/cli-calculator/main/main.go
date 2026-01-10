a17957d8f70ffcb8816700caf941eae13a64c889
sarveshkyndryl-a11y <sarveshkyndryl@gmail.com>
2026-01-10 13:45:42 +0530

    cli-calculator done

--- Projects/cli-calculator/main/main.go
+++ Projects/cli-calculator/main/main.go
@@ -1,6 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+var rules string = "One operation: --add,--sub,--mul--divi with two numbers //Eg: --add 12 13"
 func main(){
-	fmt.Println("hello")
+	args := os.Args
+	if len(args)!=4{
+		fmt.Println(rules)
+		// fmt.Print(len(args))
+		return
+	}
+	operation := args[1]
+	firstnumber := args[2]
+	secondnumber := args[3]
+
+	if operation != "--add" && operation != "--sub" &&
+		operation != "--mul" && operation != "--div" {
+		fmt.Println(rules)
+		return
+	}
+	num1,err1  := strconv.Atoi(firstnumber)
+	num2,err2  := strconv.Atoi(secondnumber)
+	if err1 !=nil || err2!=nil{
+		fmt.Println("Error: please enter valid numbers")
+		return
+	}
+
+
+	switch operation{
+	case "--add":
+		fmt.Println("****************")
+		fmt.Println("")
+		fmt.Printf("Your Calculated Value is %v \n",num1+num2)
+		fmt.Println("")
+		fmt.Println("****************")
+	case "--sub":
+		fmt.Println("****************")
+		fmt.Println("")
+		fmt.Printf("Your Calculated Value is %v \n",num1-num2)
+		fmt.Println("")
+		fmt.Println("****************")
+	case "--mul":
+		fmt.Println("****************")
+		fmt.Println("")
+		fmt.Printf("Your Calculated Value is %v \n",num1*num2)
+		fmt.Println("")
+		fmt.Println("****************")
+	case "--div":
+		if num2 == 0{
+			fmt.Println("Division by zero error")
+			
+			return
+		}
+		fmt.Println("****************")
+		fmt.Println("")
+		fmt.Printf("Your Calculated Value is %v \n",num1/num2)
+		fmt.Println("")
+		fmt.Println("****************")
+
+	}
+	fmt.Println("Program done")
+	
+	
+	
 }
